2a26506a84b6430580c831649a6ba2e5343527f5
bbedry <bedri@amani.tech>
2026-06-22 17:44:36 +0300

    release: 1.3.13

--- Sources/AmaniUI/AmaniUI.swift
+++ Sources/AmaniUI/AmaniUI.swift
@@ -31,7 +31,7 @@ public class AmaniUI {
   internal var voiceAssistant: AmaniVoiceAssistant?
 #endif
   internal let sharedSDKInstance = Amani.sharedInstance
-  private let version = "1.3.11"
+  private let version = "1.3.13"
   
   var missingRules:[[String:String]]? = nil
   var rulesKYC: [KYCRuleModel] = []
